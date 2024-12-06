c9b16ae29cc7f48456ecbfbdd9c2f7b264dab52e
burakegekocabay <burakegekocabay@outlook.com>
2024-12-06 22:08:44 +0300

    required module updates

--- library/src/main/java/module-info.java
+++ library/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.library {
     requires javafx.controls;
+    requires transitive javafx.graphics;
     requires javafx.fxml;
-    requires java.sql;
+    requires transitive java.sql;
     
     opens com.library to javafx.fxml;
     exports com.library;
